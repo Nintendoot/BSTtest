d1f561a6203def0d3f8b0b7a9c365a1a737a4ddc
olegtereshkov <nintendoot@gmail.com>
2021-07-03 19:08:57 +0300

    Done WorkersController.

--- crud-module/src/main/java/by/nintendo/controller/WorkersController.java
+++ crud-module/src/main/java/by/nintendo/controller/WorkersController.java
@@ -4,27 +4,23 @@ import by.nintendo.Status;
 import by.nintendo.entity.WorkerEntity;
 import by.nintendo.mapper.WorkerMapper;
 import by.nintendo.model.WorkerModel;
-import by.nintendo.repository.WorkersRepository;
 import by.nintendo.service.WorkersService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 import by.nintendo.Response;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping(path = "/workers")
 public class WorkersController {
     private final WorkersService workersService;
     private final WorkerMapper workerMapper;
-@Autowired
-private WorkersRepository repository;
 
     public WorkersController(WorkersService workersService, WorkerMapper workerMapper) {
         this.workersService = workersService;
@@ -45,7 +41,7 @@ private WorkersRepository repository;
 
             response.setStatus(stringBuilder.toString());
             response.setEntities(Collections.singletonList(workerMapper.toModel(workerEntity)));
-            log.info("POST request hasErrors."+stringBuilder.toString());
+            log.info("POST request hasErrors." + stringBuilder.toString());
         } else {
             WorkerModel worker = workersService.createWorker(workerEntity);
             response.setStatus(Status.CREATED.getName());
@@ -58,33 +54,30 @@ private WorkersRepository repository;
     public Response<WorkerModel> getAll() {
         log.info("GET request /workers");
         Response<WorkerModel> response = new Response<>();
-        if(workersService.getAll().isEmpty()){
+        if (workersService.getAll().isEmpty()) {
             log.info("GET request hasErrors.");
             response.setStatus(Status.BAD.getName());
         }
         response.setStatus(Status.OK.getName());
-        List<WorkerModel> list=workersService.getAll();
+        List<WorkerModel> list = workersService.getAll();
         response.setEntities(list);
         return response;
     }
 
     @GetMapping(path = "/{id}")
     public Response<WorkerModel> getWorkerById(@PathVariable("id") Long id) {
+        log.info("GET request /workers/" + id);
         Response<WorkerModel> response = new Response<>();
-
         response.setStatus(Status.OK.getName());
-        WorkerModel workerModel = workerMapper.toModel(workersService.getById(id));
-        response.setEntities(Collections.singletonList(workerModel));
-
+        response.setEntities(Collections.singletonList(workersService.getById(id)));
         return response;
     }
 
     @DeleteMapping(path = "/{id}")
-    public void deleteWorkerById(@PathVariable("id") Long id) {
-      workersService.deleteById(id);
-//        return new Response<>(Status.DELETE.getName(), Collections.singletonList(worker) );
+    public Response<WorkerModel> deleteWorkerById(@PathVariable("id") Long id) {
+        log.info("DELETE request /workers/" + id);
+        WorkerModel workerModel = workersService.deleteById(id);
+        return new Response<>(Status.DELETE.getName(), Collections.singletonList(workerModel));
     }
-
-
 }
 
--- crud-module/src/main/java/by/nintendo/service/WorkersService.java
+++ crud-module/src/main/java/by/nintendo/service/WorkersService.java
@@ -6,7 +6,6 @@ import by.nintendo.mapper.WorkerMapper;
 import by.nintendo.model.WorkerModel;
 import by.nintendo.repository.WorkersRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,25 +30,28 @@ public class WorkersService {
     }
 
     public List<WorkerModel> getAll() {
+        log.info("Call method: getAll()");
         return workersRepository.findAll().stream().map(workerMapper::toModel).collect(Collectors.toList());
     }
-    public WorkerEntity getById(Long id){
+
+    public WorkerModel getById(Long id) {
+        log.info("Call method: getById(Id: " + id + ") ");
         Optional<WorkerEntity> worker = workersRepository.findById(id);
-        if(worker.isPresent()){
-            return worker.get();
-        }else{
+        if (worker.isPresent()) {
+            return workerMapper.toModel(worker.get());
+        } else {
             throw new WorkerNotFoundException("Worker not found.");
         }
-
     }
 
-    public WorkerModel deleteById(Long id){
+    public WorkerModel deleteById(Long id) {
+        log.info("Call method: deleteById(Id: " + id + ") ");
         Optional<WorkerEntity> worker = workersRepository.findById(id);
-        if(worker.isPresent()){
+        if (worker.isPresent()) {
             WorkerModel workerModel = workerMapper.toModel(worker.get());
             workersRepository.deleteById(id);
             return workerModel;
-        }else{
+        } else {
             throw new WorkerNotFoundException("Worker not found.");
         }
     }
